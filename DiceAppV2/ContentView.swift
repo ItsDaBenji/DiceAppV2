8928931527b9e9f2e40da079b01028ae9eba6aaf
ItsDaBenji <benmblackmon@gmail.com>
2022-11-20 16:43:14 -0600

    Initial Commit

---
+++ DiceAppV2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiceAppV2
+//
+//  Created by Benjamin Blackmon on 11/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceAppV2/DiceAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppV2App.swift
+//  DiceAppV2
+//
+//  Created by Benjamin Blackmon on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
